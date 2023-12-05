adeb2adfd00409e9df27805bd2bc149302d55101
Dawid-Stankowski <dawid441@interia.pl>
2023-12-05 07:12:27 +0100

    1.0

--- src/Osoba.java
+++ src/Osoba.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Osoba {
+public class Osoba {
+    private String imie;
+    private String nazwisko;
+    private int wiek;
+
+    public Osoba(String imie, String nazwisko, int wiek) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void setWiek(int wiek) {
+        this.wiek = wiek;
+    }
+
+    public void wyswietlInformacje() {
+        System.out.println("Imię: " + imie);
+        System.out.println("Nazwisko: " + nazwisko);
+        System.out.println("Wiek: " + wiek);
+    }
+    //Dodanie nowej osoby
+    public static void main(String[] args) {
+        Osoba osoba = new Osoba("Jan", "Kowalski", 30);
+        osoba.wyswietlInformacje();
+
+    }
 }
